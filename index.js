6e0fc71770f82c2dde1ed0808298ff5f7e45fd34
emmanuel-kipngeno <immanuelkipngeno7387@gmail.com>
2023-09-20 12:32:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHeadquartersInBlocks(blocks) {
+  return Math.abs(blocks - 42);
+}
+
+function distanceFromHeadquartersInFeet(blocks) {
+  let feet = distanceFromHeadquartersInBlocks(blocks);
+  return feet * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  let feetTraveled = Math.abs(start - destination);
+  return feetTraveled * 264;
+}
+
+function calculateFarePrice(start, destination) {
+  let theTravelCost = distanceTravelledInFeet(start, destination);
+  if (theTravelCost <= 400) {
+    return 0;
+  } else if (theTravelCost > 400 && theTravelCost <= 2000) {
+    return (theTravelCost - 400) * 0.02;
+  } else if (theTravelCost > 2000 && theTravelCost <= 2500) {
+    return 25;
+  } else {
+    return "Can't travel that far.";
+  }
+}
